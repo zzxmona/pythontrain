894f43d4b039ce32ba5599baf4e4d0683b9f2e4b
zzxmona <2577625924@qq.com>
2021-05-27 14:20:18 +0800

    5.27

--- 编程小白的第一本 Python 入门书/数据结构.py
+++ 编程小白的第一本 Python 入门书/数据结构.py
@@ -1,10 +1,16 @@
-all_in_list = [
-    1,  # 整数
-    1.0,  # 浮点数
-    'a word',  # 字符串
-    print(1),  # 函数
-    True,  # 布尔值
-    [1, 2],  # 嵌套列表
-    (1, 2),  # 嵌套元组
-    {'key': 'value'}  # 嵌套字典
-]
+# # 列表
+# listex = [
+#     1,  # 整数
+#     1.0,  # 浮点数
+#     'a word',  # 字符串
+#     print(1),  # 函数
+#     True,  # 布尔值
+#     [1, 2],  # 嵌套列表
+#     (1, 2),  # 嵌套元组
+#     {'key': 'value'}  # 嵌套字典
+# ]
+# print(listex)
+
+lista = []
+lista.insert(0, 'zzx')
+print(lista)
