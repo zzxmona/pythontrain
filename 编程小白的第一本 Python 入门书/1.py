07f30a8895aa1f491f665b0eded823cb1df5a571
zzxmona <2577625924@qq.com>
2021-05-27 16:06:59 +0800

    5.27

---
+++ 编程小白的第一本 Python 入门书/1.py
@@ -0,0 +1,48 @@
+import math
+
+dic = {
+    '1': 'a',
+    '2': 'b',
+    '3': 'c'
+}
+
+
+# def login():
+#     name = input()
+#     if name in dic:
+#         print(dic[name])
+#     else:
+#         print('ERROR')
+#
+# login()
+
+# name=input()
+# while name in dic:
+#     print(dic[name])
+#     break
+
+
+def prime(s):
+    if s < 2:
+        return False
+    else:
+        for i in range(2, s):
+            if s % i == 0:
+                return False
+        else:
+            return True
+
+
+ls = [1, 2, 3, 4, 5, 6]
+for item in ls[:]:
+    if prime(item):
+        ls.remove(item)
+        print(id(ls[:]), id(ls))
+
+print(ls)
+
+a = int(input())
+if prime(a):
+    print('YES!')
+else:
+    print('ERROR!')
--- 编程小白的第一本 Python 入门书/数据结构.py
+++ 编程小白的第一本 Python 入门书/数据结构.py
@@ -1,16 +1,56 @@
-# # 列表
-# listex = [
-#     1,  # 整数
-#     1.0,  # 浮点数
-#     'a word',  # 字符串
-#     print(1),  # 函数
-#     True,  # 布尔值
-#     [1, 2],  # 嵌套列表
-#     (1, 2),  # 嵌套元组
-#     {'key': 'value'}  # 嵌套字典
-# ]
-# print(listex)
+# 列表
+def print1(a):
+    return a
 
-lista = []
-lista.insert(0, 'zzx')
+
+listex = [
+    1,  # 整数
+    1.0,  # 浮点数
+    'a word',  # 字符串
+    print1('zzx'),  # 函数
+    True,  # 布尔值
+    [1, 2],  # 嵌套列表
+    (1, 2),  # 嵌套元组
+    {'key': 'value'}  # 嵌套字典
+]
+print(listex)
+
+lista = [1]
+lista.insert(0, 'zzx')  # insert指定位置新增数据
+lista.append('zx')  # append在列表最后新增一个
+lista.extend(['x', 'z'])  # extend在列表最后新增列表
 print(lista)
+# list[a：b]选择list中a-b(不包含b)的数据 语法和字符串一样，所有选择左闭右开，若a=b则未选择
+lista[0:0] = ['nb']  # 将第0-0位置上的数据替换0：0指在第一位新增，因为没有选择,
+print(lista)
+print(lista[0:1])
+# 删除列表关键字
+del lista[0:0]  # 删除为空
+del lista[0:1]  # 删除下标为0的数据
+
+print(lista)
+# 字典
+
+dic = {
+    'BIDU': 'Baidu',
+    'SINA': 'Sina',
+    'YOKU': 'Youku'
+}
+dic2 = listex[-1]
+print(dic['BIDU'])
+print(dic2['key'])
+dic.update({'name': 'zzx', 'age': '22'})  # 通过update向字典中新增键值对
+del dic['YOKU']  # 删除元素
+print(dic)
+
+# 元组与列表相互转换，元组不可修改数据，元组查看数据方式与列表一致
+tuple = tuple(lista)
+l = list(tuple)
+print(tuple)
+print(l)
+
+#集合
+#集合会删除重复的元素，而且输出没有顺序，可以利用集合判断重属关系
+set1 = set('apple')
+set2 = set()
+print(set)
